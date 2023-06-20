62b1df5e9bf35a165cc661cc79756ce79033c22a
unknown <godhanidivyesh44@gmail.com>
2023-06-20 07:53:59 +0530

    hof file added

---
+++ Hof.js
@@ -0,0 +1,26 @@
+// HOF
+
+// A Higher-Order Function is a function that takes one or more functions as arguments and/or returns a function as its result.
+
+// Example 1:
+
+function greet(name){
+    console.log("Hello " + name + "!");
+}
+
+function inputuser(callback){
+    let name = prompt("enter your name");
+    callback(name);
+}
+inputuser(greet);
+
+// Example 2:
+
+function mutiplyby(factor){
+    return function(number){
+        return number * factor;
+    }
+}
+
+let double = mutiplyby(2);
+console.log(double(5));
