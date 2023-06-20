e7fe87a3adae6f67b2fb234b31c0534bd6ade40d
unknown <godhanidivyesh44@gmail.com>
2023-06-20 09:51:54 +0530

    memory management file add

---
+++ Memory.js
@@ -0,0 +1,15 @@
+// Memory
+
+// memory refers to the space where values, variables, objects, and other data are stored during program execution. JavaScript has an automatic memory management system that handles the allocation and deallocation of memory for you.
+
+var name = "nomy";
+var age = 50;
+var number = [1,2,3,4,5];
+var person = { name :"john",age : 25}
+
+function greet(){
+    var message = "Hello " + name + "!";
+    console.log(message);
+}
+greet();
+
